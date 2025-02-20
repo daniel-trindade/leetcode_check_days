0ba2a75027513e5dc4918e1be0ae94928cfe72e8
daniel-trindade <danielbrunotrindade@gmail.com>
2025-02-20 14:33:52 -0300

    challenge 1980 checked

---
+++ challenge_1980.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+string findDifferentBinaryString(vector<string>& nums) {
+  int n = nums.size();
+  string result = "";
+  
+  for (int i = 0; i < n; i++) {
+    result += (nums[i][i] == '0') ? '1' : '0';
+  }
+  
+  return result;
+        
+}
+
+
+int main(){
+  vector<string> nums = {"01","10"};
+  cout << findDifferentBinaryString(nums);
+
+}
+
